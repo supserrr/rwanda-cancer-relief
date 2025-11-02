d461df2e3c4533768073cd4eacc73db67ea9804f
supserrr <s.shima@alustudent.com>
2025-11-02 18:51:24 +0200

    Revert "chore: update eslint config to allow underscore-prefixed unused vars"
    This reverts commit f67257b6f5b9bc396852e83c802ad3aba08e2055.

--- frontend/apps/web/eslint.config.js
+++ frontend/apps/web/eslint.config.js
@@ -1,19 +1,4 @@
 import { nextJsConfig } from "@workspace/eslint-config/next-js"
 
 /** @type {import("eslint").Linter.Config} */
-export default [
-  ...nextJsConfig,
-  {
-    rules: {
-      // Allow variables and arguments that start with underscore
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        {
-          "argsIgnorePattern": "^_",
-          "varsIgnorePattern": "^_",
-          "caughtErrorsIgnorePattern": "^_"
-        }
-      ],
-    }
-  }
-]
+export default nextJsConfig
